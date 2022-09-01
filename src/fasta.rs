1dd921625143099abaf23ef61c2a4c40d1ff2f6b
Alyssa Rose <alyssarosedev@proton.me>
2022-08-31 21:34:28 -0700

    improve fasta parsing

--- src/fasta.rs
+++ src/fasta.rs
@@ -4,9 +4,8 @@ use nom::{
     bytes::complete::take_until,
     character::complete::{newline, one_of},
     sequence::delimited,
-    IResult, Parser,
+    Parser,
 };
-use nom_supreme::error::ErrorTree;
 use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 use std::{ops::Index, str::FromStr};
 
@@ -31,17 +30,11 @@ impl<T: Sequence> Fasta<T> {
     pub fn parse(src: &str) -> TXaseResult<Either<Self, Vec<Self>>> {
         let mut seqs: Vec<Fasta<T>> = crate::util::Split2Iter::new(b'>', b';', src)
             .map(|seq| {
-                if let Ok((comment, seq_rem)) = comment(seq) {
-                    Ok(Self {
-                        description: Some(comment.into()),
-                        sequence: T::parse(seq_rem)?,
-                    })
-                } else {
-                    Ok(Self {
-                        description: None,
-                        sequence: T::parse(seq)?,
-                    })
-                }
+                let (d, s) = comment(seq);
+                Ok(Self {
+                    description: d.map(Into::into),
+                    sequence: T::from_str(s)?,
+                })
             })
             .collect::<TXaseResult<Vec<Fasta<T>>>>()?;
         if seqs.len() == 1 {
@@ -63,17 +56,11 @@ impl<T: Sequence + Send> Fasta<T> {
         let mut seqs: Vec<Fasta<T>> = crate::util::Split2Iter::new(b'>', b';', src)
             .into_par_iter()
             .map(|seq| {
-                if let Ok((comment, seq_rem)) = comment(seq) {
-                    Ok(Self {
-                        description: Some(comment.into()),
-                        sequence: seq_rem.parse()?,
-                    })
-                } else {
-                    Ok(Self {
-                        description: None,
-                        sequence: src.parse()?,
-                    })
-                }
+                let (d, s) = comment(seq);
+                Ok(Self {
+                    description: d.map(Into::into),
+                    sequence: T::from_str(s)?,
+                })
             })
             .collect::<TXaseResult<Vec<Fasta<T>>>>()?;
         if seqs.len() == 1 {
@@ -84,8 +71,12 @@ impl<T: Sequence + Send> Fasta<T> {
     }
 }
 
-pub(crate) fn comment(src: &str) -> IResult<&str, &str, ErrorTree<&str>> {
-    delimited(one_of(">;"), take_until("\n"), newline).parse(src)
+pub(crate) fn comment(src: &str) -> (Option<&str>, &str) {
+    delimited::<_, _, _, _, (), _, _, _>(one_of(">;"), take_until("\n"), newline)
+        .parse(src)
+        .ok()
+        .map(|(comment, seq)| (Some(comment), seq))
+        .unwrap_or_else(|| (None, src))
 }
 
 /// A sequence that can be:
